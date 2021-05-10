578993be0231c178dddac4fbdb41d58061a898f7
pawelski3 <pawelski3@o2.pl>
2021-05-10 20:44:53 +0200

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const path=require("path");
-//var cors = require('cors');
+var cors = require('cors');
 
 const app=express();
-//app.use(cors());
+app.use(cors());
 app.use(express.static('./dist/mean-app'));
 
 app.get("/*",(req,res)=>
   res.sendFile('index.html',{root:'dist/mean-app/'})
 )
 
-app.listen(process.env.PORT||8080)
+app.listen(process.env.PORT||8000)

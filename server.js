8a98633a1a3a59ba4084b9da800a72391fc3c29e
pawelski3 <pawelski3@o2.pl>
2021-05-10 21:32:38 +0200

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const path=require("path");
-var cors = require('cors');
+const cors = require('cors');
+const bodyParser=require('body-parser');
+
 
 const app=express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('./dist/mean-app'));
 
@@ -10,4 +14,4 @@ app.get("/*",(req,res)=>
   res.sendFile('index.html',{root:'dist/mean-app/'})
 )
 
-app.listen(process.env.PORT||8000)
+app.listen(process.env.PORT||5000)

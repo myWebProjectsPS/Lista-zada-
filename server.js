aeb8fd6eebf02d6f151bb7641e91fb9bf23edbcf
pawelski3 <pawelski3@o2.pl>
2021-05-10 17:28:41 +0200

    s2

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app=express();
 app.use(cors());
 app.use(express.static('./dist/mean-app'));
 
-app.get("/*",(req,res)=>{
+app.get("/*",(req,res)=>
   res.sendFile('index.html',{root:'dist/mean-app/'})
-})
+)
 
 app.listen(process.env.PORT||8080)

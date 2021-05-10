20013f8676e0beedcccf2e9d558cd799c55711af
pawelski3 <pawelski3@o2.pl>
2021-05-10 17:37:48 +0200

    s2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const path=require("path");
-var cors = require('cors');
+//var cors = require('cors');
 
 const app=express();
-app.use(cors());
+//app.use(cors());
 app.use(express.static('./dist/mean-app'));
 
 app.get("/*",(req,res)=>
